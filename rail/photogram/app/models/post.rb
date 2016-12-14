62cc813503ef71c7580c70273bd833ecd96822d2
aadii104 <sharmaadi104@gmail.com>
2016-12-14 23:23:11 +0530

    photogram-1

---
+++ rail/photogram/app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+      belongs_to :user
+      validates :image, presence: true
+      validates :user_id, presence: true
+
+      has_attached_file :image, styles: { medium: "640x640>", thumb: "100x100>" }
+      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
